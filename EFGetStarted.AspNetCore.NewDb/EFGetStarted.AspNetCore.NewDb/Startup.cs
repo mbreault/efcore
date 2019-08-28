da44dd422005a22febb60c5ddea2790b7dacf4c2
Mike Breault <mikeb@mcrtechnologies.com>
2019-08-28 13:17:37 -0400

    added EF entities

--- EFGetStarted.AspNetCore.NewDb/EFGetStarted.AspNetCore.NewDb/Startup.cs
+++ EFGetStarted.AspNetCore.NewDb/EFGetStarted.AspNetCore.NewDb/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using EFGetStarted.AspNetCore.NewDb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFGetStarted.AspNetCore.NewDb
 {
@@ -33,6 +35,14 @@ namespace EFGetStarted.AspNetCore.NewDb
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connection = @"Server=.;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<BloggingContext>
+                (options => options.UseSqlServer(connection));
+            // BloggingContext requires
+            // using EFGetStarted.AspNetCore.NewDb.Models;
+            // UseSqlServer requires
+            // using Microsoft.EntityFrameworkCore;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
